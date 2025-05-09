21aefda5ebc0c19c3ab1acdbfc5a746d1fa39999
JulianCSN <julian.csn27@gmail.com>
2025-05-09 12:45:53 -0600

    fix width and height of clancy text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,8 +51,8 @@ export default function Home() {
           <Image
             src="/images/clancyVerticalRed.webp"
             alt="Clancy Text"
-            width={100}
-            height={100}
+            width={80}
+            height={80}
             priority
             quality={100}
           />
