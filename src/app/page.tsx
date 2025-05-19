c74bf3be393bded4b76e3522fc84f435034ee693
JulianCSN <julian.csn27@gmail.com>
2025-05-19 11:24:54 -0600

    Create InitAlbumCover component with parallax effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Lenis from "lenis";
 
 import IntroLogo from "@/components/IntroLogo";
 import ClancyTape from "@/components/ClancyTape";
+import InitAlbumCover from "@/components/InitAlbumCover";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -50,36 +51,7 @@ export default function Home() {
       <ClancyTape repeat={50} position="right" />
       <IntroLogo />
       <div style={{height: "400vh"}}>
-        <div className="flex flex-col items-center justify-center mb-20 lg:mb-44">
-          <Image
-            src="/images/ClancyTitle.png"
-            alt="background-clancy"
-            width={1500}
-            height={1500}
-            quality={100}
-            className="w-[35vh] sm:w-[50vh] md:w-[60vh] xl:w-[90vh]"
-          />
-        </div>
-        <div className="relative w-full h-[70vh] sm:h-[120vh]">
-          {/* Backround image */}
-          <Image
-            src="/images/backgroundFire.jpg"
-            alt="background-clancy"
-            width={2400}
-            height={1516}
-            quality={100}
-            className="w-full h-full object-cover"
-          />
-          {/* Tyler and Josh Clancy Cover */}
-          <Image
-            src="/images/clancyboys.png"
-            alt="Tyler and josh clancy cover"
-            width={2400}
-            height={1516}
-            quality={100}
-            className="absolute bottom-0 right-0 w-[120vh]"
-          />
-        </div>
+        <InitAlbumCover />
       </div>
     </>
   );
---
+++ src/components/InitAlbumCover.tsx
@@ -0,0 +1,78 @@
+import React, {useEffect} from "react";
+import Image from "next/image";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function InitAlbumCover() {
+  // Parallax effect
+  useEffect(() => {
+    gsap.to("#background-parallax", {
+      y: 150,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#background-parallax",
+        start: "top bottom",
+        end: "+=2000",
+        scrub: 0.5,
+      },
+    });
+
+    gsap.to("#cover-parallax", {
+      y: -60,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#cover-parallax",
+        start: "top bottom",
+        end: "+=2000",
+        scrub: 0.5,
+      },
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center lg:mb-16">
+        <Image
+          src="/images/ClancyTitle.png"
+          alt="background-clancy"
+          width={1500}
+          height={1500}
+          quality={100}
+          className="w-[35vh] sm:w-[50vh] md:w-[60vh] xl:w-[90vh]"
+        />
+      </div>
+
+      {/* Album Cover image */}
+      <div className="relative w-full h-[65vh] sm:h-[120vh] album-container">
+        {/* Top and bottom borders */}
+        <div className="album-border-top" />
+        <div className="album-border-bottom" />
+
+        {/* Background image with parallax */}
+        <Image
+          src="/images/backgroundFire.jpg"
+          alt="background"
+          width={2400}
+          height={1516}
+          quality={100}
+          className="w-full h-full object-cover will-change-transform"
+          style={{zIndex: 0}}
+          id="background-parallax"
+        />
+
+        {/* Tyler and Josh image with parallax */}
+        <Image
+          src="/images/clancyboys.png"
+          alt="Tyler and Josh Clancy Cover"
+          width={2400}
+          height={1516}
+          quality={100}
+          className="absolute bottom-0 right-0 w-[110vh] will-change-transform"
+          id="cover-parallax"
+        />
+      </div>
+    </>
+  );
+}
