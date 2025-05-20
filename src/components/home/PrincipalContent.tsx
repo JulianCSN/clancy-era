9cb1bb12e203d0664057552a34325f811e67260c
JulianCSN <julian.csn27@gmail.com>
2025-05-19 21:11:48 -0600

    create main component for home page

---
+++ src/components/home/PrincipalContent.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {use, useEffect, useRef} from "react";
+import Image from "next/image";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+
+import IntroLogo from "@/components/IntroLogo";
+import ClancyTape from "@/components/ClancyTape";
+import InitAlbumCover from "@/components/InitAlbumCover";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function PrincipalContent() {
+  // Change smoothly the background color, white to black
+  useEffect(() => {
+    // Indicate scroll position to top of the page always at start
+    window.scrollTo(0, 0);
+
+    gsap.fromTo(
+      document.body,
+      {
+        backgroundColor: "#231f20",
+      },
+      {
+        backgroundColor: "#231f20",
+        scrollTrigger: {
+          trigger: document.body,
+          start: "top top",
+          end: "+=1000",
+          scrub: 0.5,
+        },
+      }
+    );
+  }, []);
+
+  // Lenis syncrinization with GSAP
+  useEffect(() => {
+    const lenis = new Lenis();
+    lenis.on("scroll", ScrollTrigger.update);
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+    });
+    gsap.ticker.lagSmoothing(0);
+  }, []);
+
+  return (
+    <>
+      <ClancyTape repeat={50} position="left" />
+      <ClancyTape repeat={50} position="right" />
+      <IntroLogo />
+      <div style={{height: "400vh"}}>
+        <InitAlbumCover />
+      </div>
+    </>
+  );
+}
