ce5b6b2aedec774a5c6506017b3430356872a524
JulianCSN <julian.csn27@gmail.com>
2025-05-25 23:51:03 -0600

    fix: font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type {Metadata} from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Twenty One Pilots | Clancy",
   description: "Discover the Clancy Era of Twenty One Pilots!",
 };
+
+const martianMono = localFont({
+  src: "./fonts/MartianMono.ttf",
+  variable: "--font-martianMono",
+  weight: "100 900",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased overflow-x-hidden`}>{children}</body>
+      <body
+        className={`antialiased overflow-x-hidden ${martianMono.variable} font-[family-name:var(--font-martianMono)]`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
